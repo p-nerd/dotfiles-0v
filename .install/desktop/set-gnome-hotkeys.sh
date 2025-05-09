7f930a99027780bd7a774da0cc69aee4a3173d79
Shihab Mahamud <shihab4t@gmail.com>
2025-05-10 03:03:36 +0600

    set gnome hotkeys

--- .install/desktop/set-gnome-hotkeys.sh
+++ .install/desktop/set-gnome-hotkeys.sh
@@ -18,15 +18,15 @@ gsettings set org.gnome.mutter dynamic-workspaces false
 gsettings set org.gnome.desktop.wm.preferences num-workspaces 6
 
 # Use alt for pinned apps
-# gsettings set org.gnome.shell.keybindings switch-to-application-1 "['<Alt>1']"
-# gsettings set org.gnome.shell.keybindings switch-to-application-2 "['<Alt>2']"
-# gsettings set org.gnome.shell.keybindings switch-to-application-3 "['<Alt>3']"
-# gsettings set org.gnome.shell.keybindings switch-to-application-4 "['<Alt>4']"
-# gsettings set org.gnome.shell.keybindings switch-to-application-5 "['<Alt>5']"
-# gsettings set org.gnome.shell.keybindings switch-to-application-6 "['<Alt>6']"
-# gsettings set org.gnome.shell.keybindings switch-to-application-7 "['<Alt>7']"
-# gsettings set org.gnome.shell.keybindings switch-to-application-8 "['<Alt>8']"
-# gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Alt>9']"
+gsettings set org.gnome.shell.keybindings switch-to-application-1 "['<Super>1']"
+gsettings set org.gnome.shell.keybindings switch-to-application-2 "['<Super>2']"
+gsettings set org.gnome.shell.keybindings switch-to-application-3 "['<Super>3']"
+gsettings set org.gnome.shell.keybindings switch-to-application-4 "['<Super>4']"
+gsettings set org.gnome.shell.keybindings switch-to-application-5 "['<Super>5']"
+gsettings set org.gnome.shell.keybindings switch-to-application-6 "['<Super>6']"
+gsettings set org.gnome.shell.keybindings switch-to-application-7 "['<Super>7']"
+gsettings set org.gnome.shell.keybindings switch-to-application-8 "['<Super>8']"
+gsettings set org.gnome.shell.keybindings switch-to-application-9 "['<Super>9']"
 
 # Use super for workspaces
 gsettings set org.gnome.desktop.wm.keybindings switch-to-workspace-1 "['<Alt>1']"
