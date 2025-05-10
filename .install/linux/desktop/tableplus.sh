c477e52857a12a816ee4491048ca2e320f421b69
Shihab Mahamud <shihab4t@gmail.com>
2025-05-10 14:49:26 +0600

    update

---
+++ .install/linux/desktop/tableplus.sh
@@ -0,0 +1,12 @@
+# Ubuntu 24.04 X86_64
+# https://tableplus.com/download/linux
+
+# Add TablePlus gpg key
+wget -qO - https://deb.tableplus.com/apt.tableplus.com.gpg.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/tableplus-archive.gpg >/dev/null
+
+# Add TablePlus repo
+sudo add-apt-repository "deb [arch=amd64] https://deb.tableplus.com/debian/24 tableplus main"
+
+# Install
+sudo apt update
+sudo apt install tableplus
